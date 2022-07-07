b876b7be111d62bd29eaa168929e234b729d10bc
Jean-Philippe MAGIDO <jpmagido@me.com>
2022-07-07 20:53:32 +0200

    JPM - add more specs

--- spec/lib/website_generator_spec.rb
+++ spec/lib/website_generator_spec.rb
@@ -2,10 +2,14 @@ require 'rspec'
 require_relative '../../lib/website_generator'
 
 RSpec.describe WebsiteGenerator do
-  let(:youtube_generator) { described_class.new('youtube', 'boxe francaise') }
-  let(:google_generator) { described_class.new('google', 'boxe francaise') }
-  let(:amazon_generator) { described_class.new('amazon', 'boxe francaise') }
-  let(:github_generator) { described_class.new('github', 'boxe francaise') }
+  let(:youtube_boxe) { described_class.new('youtube', 'boxe francaise') }
+  let(:youtube_cs) { described_class.new('youtube', 'counter strike') }
+  let(:google_boxe) { described_class.new('google', 'boxe francaise') }
+  let(:google_cs) { described_class.new('google', 'counter strike') }
+  let(:amazon_boxe) { described_class.new('amazon', 'boxe francaise') }
+  let(:amazon_cs) { described_class.new('amazon', 'counter strike') }
+  let(:github_boxe) { described_class.new('github', 'boxe francaise') }
+  let(:github_cs) { described_class.new('github', 'counter strike') }
 
   context 'shared tests' do
     random_website_name = %w(youtube github amazon google).sample
@@ -15,19 +19,23 @@ RSpec.describe WebsiteGenerator do
   end
 
   context 'youtube generator' do
-    it { expect(youtube_generator.url).to eq 'https://www.youtube.com/results?search_query=boxe+francaise' }
+    it { expect(youtube_boxe.url).to eq 'https://www.youtube.com/results?search_query=boxe+francaise' }
+    it { expect(youtube_cs.url).to eq 'https://www.youtube.com/results?search_query=counter+strike' }
   end
 
   context 'google generator' do
-    it { expect(google_generator.url).to eq 'https://www.google.com/search?q=boxe+francaise' }
+    it { expect(google_boxe.url).to eq 'https://www.google.com/search?q=boxe+francaise' }
+    it { expect(google_cs.url).to eq 'https://www.google.com/search?q=counter+strike' }
   end
 
   context 'amazon generator' do
-    it { expect(amazon_generator.url).to eq 'https://www.amazon.fr/s?k=boxe+francaise' }
+    it { expect(amazon_boxe.url).to eq 'https://www.amazon.fr/s?k=boxe+francaise' }
+    it { expect(amazon_cs.url).to eq 'https://www.amazon.fr/s?k=counter+strike' }
   end
 
   context 'github generator' do
-    it { expect(github_generator.url).to eq 'https://github.com/search?q=boxe+francaise' }
+    it { expect(github_boxe.url).to eq 'https://github.com/search?q=boxe+francaise' }
+    it { expect(github_cs.url).to eq 'https://github.com/search?q=counter+strike' }
   end
 
   context 'when wrong input' do
